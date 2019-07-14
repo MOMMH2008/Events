3f210523d875d1050a2bc504d3783325aaa56791
Mohamed Helmy <mommh2008@gmail.com>
2019-07-14 14:19:54 +0200

    Init the Project

---
+++ Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Mohamed Helmy on 7/14/19.
+//  Copyright © 2019 MohamedHelmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
